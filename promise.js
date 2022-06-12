f885eb206fb721ebdb01274058c7f841cbc75012
Mohamad Saefudin <thismohamads@gmail.com>
2022-06-12 20:42:36 +0700

    feat : Challenge Promise 152235865101650 - Mohamad Saefudin

--- promise.js
+++ promise.js
@@ -1,7 +1,22 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  try {
+    const theaterXxi = await promiseTheaterIXX();
+    const theaterVgc = await promiseTheaterVGC();
+
+    const concatData = theaterXxi.concat(theaterVgc);
+
+    const result = concatData.filter(
+      element => (element.hasil === emosi)
+    )
+
+    return result.length;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 module.exports = {
   promiseOutput,
